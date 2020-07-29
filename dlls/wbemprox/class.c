7c82efa494770fde3fc080d72b9e85ec1e9eaae2
Alexandre Abgrall <aabgrall98@gmail.com>
2020-07-29 12:44:26 +0200

    wbemprox: Temp fix (3rd re-push): add NULL checks for strings and init record_logicaldisk to zero

--- dlls/wbemprox/class.c
+++ dlls/wbemprox/class.c
@@ -344,6 +344,7 @@ static HRESULT WINAPI class_object_GetQualifierSet(
     return WbemQualifierSet_create( co->name, NULL, (void **)ppQualSet );
 }
 
+
 static HRESULT record_get_value( const struct record *record, UINT index, VARIANT *var, CIMTYPE *type )
 {
     VARTYPE vartype = to_vartype( record->fields[index].type & CIM_TYPE_MASK );
@@ -361,7 +362,13 @@ static HRESULT record_get_value( const struct record *record, UINT index, VARIAN
     case CIM_STRING:
     case CIM_DATETIME:
     case CIM_REFERENCE:
-        V_BSTR( var ) = SysAllocString( record->fields[index].u.sval );
+        if (!record->fields[index].u.sval)
+        {
+            V_BSTR( var ) = NULL;
+            FIXME("sval is NULL\n");
+        }
+        else
+            V_BSTR( var ) = SysAllocString( record->fields[index].u.sval );
         break;
     case CIM_SINT32:
         V_I4( var ) = record->fields[index].u.ival;
